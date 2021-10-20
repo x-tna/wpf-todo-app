6fb0fc48c1fa1522151612b8b7f88a3e25af17ac
Christina Feichtenschlager <christina.feichtenschlager@krones.com>
2021-10-20 11:22:17 +0200

    Überflüssige NugetPakete entfernt

--- ToDo/ViewModels/MainWindowViewModel.cs
+++ ToDo/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,6 @@
-﻿using Newtonsoft.Json;
-using Prism.Commands;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using ToDo.Commands;
 using ToDo.Services;
 
