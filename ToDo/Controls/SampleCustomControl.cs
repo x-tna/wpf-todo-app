02b8ecf8a7cf22ecb9404380d9788193ac124a14
Christina Feichtenschlager <christina.feichtenschlager@krones.com>
2021-11-02 09:44:13 +0100

    SampleCustomControl angelegt

--- ToDo/Controls/SampleCustomControl.cs
+++ ToDo/Controls/SampleCustomControl.cs
@@ -5,38 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ToDo.Controls
 {
-    public partial class TextBoxButtonWithHeaderControl : UserControl
+
+
+    public class SampleCustomControl : Control
     {
-
-
         public static readonly DependencyProperty HeaderProperty =
-            DependencyProperty.Register("Header", 
-                typeof(string), typeof(TextBoxButtonWithHeaderControl), 
-                new PropertyMetadata("Neuen Eintrag hinzufügen"));
+            DependencyProperty.Register("Header",
+        typeof(string), typeof(SampleCustomControl),
+        new PropertyMetadata("Neuen Eintrag hinzufügen"));
 
         public static readonly DependencyProperty ButtonContentProperty =
-            DependencyProperty.Register("ButtonContent", 
-                typeof(string), typeof(TextBoxButtonWithHeaderControl), 
+            DependencyProperty.Register("ButtonContent",
+                typeof(string), typeof(SampleCustomControl),
                 new PropertyMetadata("Hinzufügen"));
 
         public static readonly DependencyProperty ButtonCommandProperty =
-            DependencyProperty.Register("ButtonCommand", 
-                typeof(ICommand), typeof(TextBoxButtonWithHeaderControl), 
+            DependencyProperty.Register("ButtonCommand",
+                typeof(ICommand), typeof(SampleCustomControl),
                 new PropertyMetadata());
 
         public static readonly DependencyProperty TextBoxTextProperty =
-            DependencyProperty.Register("TextBoxText", 
-                typeof(string), typeof(TextBoxButtonWithHeaderControl),
+            DependencyProperty.Register("TextBoxText",
+                typeof(string), typeof(SampleCustomControl),
                 new PropertyMetadata("Neues Todo"));
 
 
@@ -64,11 +58,9 @@ namespace ToDo.Controls
             set { SetValue(TextBoxTextProperty, value); }
         }
 
-
-
-        public TextBoxButtonWithHeaderControl()
+        static SampleCustomControl()
         {
-            InitializeComponent();
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SampleCustomControl), new FrameworkPropertyMetadata(typeof(SampleCustomControl)));
         }
     }
 }
---
+++ ToDo/Controls/SampleUserControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDo.Controls
+{
+    public partial class SampleUserControl : UserControl
+    {
+
+        public static readonly DependencyProperty HeaderUCProperty =
+            DependencyProperty.Register("HeaderUC", 
+                typeof(string), typeof(SampleUserControl), 
+                new PropertyMetadata("Neuen Eintrag hinzufügen"));
+
+        public static readonly DependencyProperty ButtonContentUCProperty =
+            DependencyProperty.Register("ButtonContentUC", 
+                typeof(string), typeof(SampleUserControl), 
+                new PropertyMetadata("Hinzufügen"));
+
+        public static readonly DependencyProperty ButtonCommandUCProperty =
+            DependencyProperty.Register("ButtonCommandUC", 
+                typeof(ICommand), typeof(SampleUserControl), 
+                new PropertyMetadata());
+
+        public static readonly DependencyProperty TextBoxTextUCProperty =
+            DependencyProperty.Register("TextBoxTextUC", 
+                typeof(string), typeof(SampleUserControl),
+                new PropertyMetadata("Neues Todo"));
+
+
+        public string HeaderUC
+        {
+            get { return (string)GetValue(HeaderUCProperty); }
+            set { SetValue(HeaderUCProperty, value); }
+        }
+
+        public string ButtonContentUC
+        {
+            get { return (string)GetValue(ButtonContentUCProperty); }
+            set { SetValue(ButtonContentUCProperty, value); }
+        }
+
+        public ICommand ButtonCommandUC
+        {
+            get { return (ICommand)GetValue(ButtonCommandUCProperty); }
+            set { SetValue(ButtonCommandUCProperty, value); }
+        }
+
+        public string TextBoxTextUC
+        {
+            get { return (string)GetValue(TextBoxTextUCProperty); }
+            set { SetValue(TextBoxTextUCProperty, value); }
+        }
+
+
+
+        public SampleUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
